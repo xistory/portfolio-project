11e09c26731444a96a7c1ba3b82e7939eaef8b0e
xiphos <ximinseo@hotmail.com>
2018-08-23 10:36:07 +0900

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create A Blog models
 # title pub_date body image
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(default=timezone.now)
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
 
 
 
